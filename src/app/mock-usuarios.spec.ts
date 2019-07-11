7ac75cffece2bb98722bc178f67c98fbe9cdcced
ELENO <goeleno@gmail.com>
2019-07-11 08:36:18 -0500

    se agrega toh-pt2 ejemplo de angular https://angular.io/generated/live-examples/toh-pt2/stackblitz.html mostrar listado mock ard code y obtener su dato una ves que lo seleccionas

---
+++ src/app/mock-usuarios.spec.ts
@@ -0,0 +1,7 @@
+import { MockUsuarios } from './mock-usuarios';
+
+describe('MockUsuarios', () => {
+  it('should create an instance', () => {
+    expect(new MockUsuarios()).toBeTruthy();
+  });
+});
--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 //importamos Clase de usuarios.ts que esta fuera de /app/
 import { Usuario } from '../usuario';
+//importamos el mock(usuarios hard-code list) de USUARIOS
+import { USUARIOSLIST } from '../mock-usuarios';
 
 @Component({
   selector: 'app-usuarios',
@@ -11,14 +13,24 @@ export class UsuariosComponent implements OnInit {
 
   
   /*usuario = 'Windstorm';*/
-  usuario: Usuario = {
+  /*usuario: Usuario = {
     id: 1,
     name: 'Martin Eleno'
-  };
+  };*/
 
+  //traemos la lista de usuarios
+  usuarioslist_ = USUARIOSLIST;
+
+  //agregamos eventos tipo  --event binding syntax.
+  selectedUsuario: Usuario;
+ 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectector(usuario: Usuario): void {
+    this.selectedUsuario = usuario;
   }
 
 }
