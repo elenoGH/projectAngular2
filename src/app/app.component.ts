56f860492f3fec8648d83b38eb4f5c667924d22b
ELENO <goeleno@gmail.com>
2019-07-15 12:22:48 -0500

    envio por post desde el frontend, con parametros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Usuarios';
+  title = 'Movimiento de Pago';
+  isCollapse = true;
+  toggleState() {
+    let foo = this.isCollapse
+    this.isCollapse = foo === false ? true : false;
+  }
 }
--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -7,9 +7,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, catchError } from "rxjs/operators";
 import 'rxjs/add/operator/map';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class UsuarioService {
 
   constructor(
@@ -17,7 +22,8 @@ export class UsuarioService {
     private http:HttpClient
     ) { }
 
-
+    
+  
   uriAllItems='http://localhost/crud/getUsuarioss';
   getUsers(): Observable<Usuario[]> {
     //this.messageService.add('UsuarioService: listado de usuarios');
@@ -31,4 +37,23 @@ export class UsuarioService {
     //return of(USUARIOSLIST.find(usuario => usuario.id === id));
     return this.getUsers().map(usuarios => usuarios.find(usuario => usuario.id == id));
   }
+  /**
+  *las siguientes funciones son para el envio de datos por URL
+  *si no sirve, borra por el momento
+  * ejemplo en: https://www.competa.com/blog/get-and-post-request-angular-4-3/
+  * seguir ejemplo https://www.devglan.com/spring-boot/spring-boot-angular-example
+  * para envo de parametros
+  */
+  
+  
+  postRequest(usuario: Usuario) : Observable<Usuario>{
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+       });
+    let options = { headers: headers };
+  
+    return this.http.post<Usuario>(`http://localhost/crud/sendMail?correo=${usuario.email}&noacount=${usuario.bancocuenta}&clavebanc=${usuario.bancoclave}&nameuser=${usuario.name}`
+    , usuario, options)
+  }
+
 }
